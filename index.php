2cf5aad6cf2cc7cf2efd8ba4f8477c79063ebce3
Carlos Herrera <harleauxcarrera@Harleauxs-MacBook-Pro.local>
2018-01-27 18:44:04 -0700

    edit

--- index.php
+++ index.php
@@ -139,7 +139,21 @@
       </form>
     </div>
 
+<?php
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+  $to = "harleauxcarrera@gmail.com";
 
+    $email_subject = "New Form submission";
+    $email_body = "You have received a new message from the user $name.\n".
+
+                            "Here is the message:\n $message".
+
+                            mail($to,$email_subject,$email_body);
+
+
+ ?>
 
 
 
