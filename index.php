de63497c1fd4529c7f46ecf13605543bc3e2b454
Carlos Herrera <harleauxcarrera@Harleauxs-MacBook-Pro.local>
2018-01-30 07:52:59 -0700

    edit img

--- index.php
+++ index.php
@@ -109,7 +109,7 @@
 
         </div>
         <div class="col-md-4">
-          <center><img class="img-fluid" src="./Pics/me.png" style="width: 100%; height:auto;"> </center>
+          <center><img class="img-fluid" src="./Pics/me.png" style="width: 100%; height:auto; border: 10px solid grey; border-radius: 10px;"> </center>
         </div>
         <div class="col-md-4">
           <p>
