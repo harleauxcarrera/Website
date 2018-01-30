3f49b50fe0e6d030fb79aa2b1f470cc3ac76485a
Carlos Herrera <harleauxcarrera@Harleauxs-MacBook-Pro.local>
2018-01-30 08:03:23 -0700

    edit img

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
   <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="main1.css">
+  <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
 
 </head>
 
