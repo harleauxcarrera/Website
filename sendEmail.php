52015478920e9a802bb30682a261c4bcce7dfdf5
Carlos Herrera <harleauxcarrera@Harleauxs-MacBook-Pro.local>
2018-02-19 14:04:03 -0700

    edit mail method

--- sendEmail.php
+++ sendEmail.php
@@ -20,17 +20,19 @@
     $to = "harleauxcarrera@gmail.com";
     $subject = "Someone's contacting me from my webpage!";
     $txt = "Sender: $name\n Email Address: $email\n Message: $message";
-    if (ctype_space($message)) {
 
-            echo "<script>
-                  document.getElementById("myHeading").innerHTML = "Please enter a valid message input";
-                  document.getElementById("title").innerHTML = "Invalid Input";
-                </script>";
-                
-                echo "only whitespace";
-      } else {
-       mail($to, $subject, $txt);
-      }
-     ?>
+
+    if (ctype_space($message)) {
+  ?>
+    <script>
+          document.getElementById("myHeading").innerHTML = "Please enter a valid message input";
+          document.getElementById("title").innerHTML = "Invalid Input";
+    </script>;
+      <?php
+        echo "only whitespace";
+          } else {
+              mail($to, $subject, $txt);
+              }
+        ?>
   </body>
 </html>
