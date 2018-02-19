42fe1e694b90b4e437b3a39eff84462471f97b2e
Carlos Herrera <harleauxcarrera@Harleauxs-MacBook-Pro.local>
2018-02-19 14:13:08 -0700

    edit mail method

--- sendEmail.php
+++ sendEmail.php
@@ -8,7 +8,7 @@
   <body>
     <div class="col-lg-12" style="text-align: center;">
       <h1 id="myHeading">Thanks! Your email has been sent</h1>
-      <button href="./index.php" class="w3-button w3-black" type="submit" >Go Back</button>
+      <a href="./index.php"><button href="./index.php" class="w3-button w3-black" type="submit" >Go Back</a>
 
     </div>
 
