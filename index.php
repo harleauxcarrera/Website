a8e4ae7e73071b82400c888d53b06b1d2b31b760
harleauxcarrera <harleauxcarrera@gmail.com>
2018-02-28 23:38:03 -0600

    edited about Me section

--- index.php
+++ index.php
@@ -117,7 +117,7 @@ padding: 36px 36px;
 
 
 <div class="bgimg-3">
-  <div class="col-md-2" style="background-color: white; margin: 20px 20px;">
+  <div class="col-md-4" style="background-color: white; ">
     <img id = "profilePic" src="Profile2.jpg" alt="">
   </div>
   <div class="overlay">
