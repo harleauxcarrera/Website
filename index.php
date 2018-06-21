2e92178dc9cdeb57ccd1be9ee9502c6eb600a8a4
Carlos Herrera <harleauxcarrera@Harleauxs-MacBook-Pro.local>
2018-06-20 22:22:59 -0600

    test

--- index.php
+++ index.php
@@ -99,7 +99,7 @@ $conn->close();
             <div class="modal-content">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h3 class="modal-title">Subscribe for news and events</h3>
+                <h3 class="modal-title">Subscribe for news and events!</h3>
               </div>
               <div class="modal-body">
 
