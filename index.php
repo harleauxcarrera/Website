7959a9332da77212deaebc6cc622aa6e45bc4c01
Carlos Herrera <harleauxcarrera@Harleauxs-MacBook-Pro.local>
2018-06-20 21:53:20 -0600

    testing php files

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
-<?php
 
-$servername = "localhost";
-$username = "harleauxcarrera";
-$password = "please313";
-$dbname = "desertNoize";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-
-
-
-
- ?>
 
 
 <html lang="en" dir="ltr">
