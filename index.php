6b264ea600708cd2cd7cceb104a14132c1bd029a
Carlos Herrera <harleauxcarrera@Harleauxs-MBP.attlocal.net>
2018-06-20 20:01:51 -0600

    added mysql

--- index.php
+++ index.php
@@ -11,19 +11,19 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+//echo "Connected successfully";
 
 
 $name = $_POST['name'];
 $email = $_POST['email'];
 
-echo $name;
-echo $email;
+//echo $name;
+//echo $email;
 
 $sql = "INSERT INTO subs (name, email) VALUES ('$name', '$email')";
 
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+  //  echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
