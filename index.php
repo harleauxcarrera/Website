9843ae4525598f735c5c5a1b5842f503feebbccd
Carlos Herrera <harleauxcarrera@Harleauxs-MacBook-Pro.local>
2018-06-20 21:44:08 -0600

    testing php files

--- index.php
+++ index.php
@@ -1,4 +1,37 @@
+<?php
 
+$servername = "localhost";
+$username = "harleauxcarrera";
+$password = "please313";
+$dbname = "desertNoize";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+
+// $name = $_POST['name'];
+// $email = $_POST['email'];
+//
+// //echo $name;
+// //echo $email;
+//
+// $sql = "INSERT INTO subs (name, email) VALUES ('$name', '$email')";
+//
+// if ($conn->query($sql) === TRUE) {
+//   //  echo "New record created successfully";
+// } else {
+//     echo "Error: " . $sql . "<br>" . $conn->error;
+// }
+//
+// $conn->close();
+
+
+ ?>
 
 
 <html lang="en" dir="ltr">
